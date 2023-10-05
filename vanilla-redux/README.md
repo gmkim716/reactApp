7c1d3830e326bc362cb2469300c9542ce955ec2b
kgm <kgm@goodit.kr>
2023-10-05 11:15:57 +0900

    update comment

--- vanilla-redux/README.md
+++ vanilla-redux/README.md
@@ -1,15 +1,20 @@
 # 16장 실습: 리덕스 라이브러리 이해하기
 
+## Summary
+1. index.js : 리듀서 함수 정의, store 만들기, render 함수 만들기, 구독하기, 액션 발생시키기
+2. 리덕스 용어 이해  
+<br>
 
 ## Memo
-- 주요 내용 요약 : index.js : 리듀서 함수 정의, store 만들기, render 함수 만들기, 구독하기, 액션 발생시키기
 - 바닐라 자바스크립트 실행 : parcel index.html
 - 리덕스 설치 : yarn add redux
 <br>
-<br>
 
 ## Knowledge
-- 리덕스 규칙 세가지<br> 1. `하나의 애플리케이션 안에는 하나의 스토어만` 사용<br> 2. 리덕스는 `읽기전용` 상태: 상태 업데이트할 때 기존 객체를 건드리지 않고 새로운 객체를 생성 -> 객체 안쪽까지 비교하는 것을 방지해서 성능을 유지<br> 3. `순수한 상태` <br> 
+#### 리덕스 규칙 세가지
+1. `하나의 애플리케이션 안에는 하나의 스토어만` 사용 
+2. 리덕스는 `읽기전용` 상태: 상태 업데이트할 때 기존 객체를 건드리지 않고 새로운 객체를 생성 -> 객체 안쪽까지 비교하는 것을 방지해서 성능을 유지 
+3. `순수한 상태`  
     - 리듀서 함수는 이전 상태, 액션 객체를 파라미터로 받아 사용
     - 파라미터 외의 값에 의존하면 안된다
     - 이전 상태는 절대 건들이지 않고 변화를 준 새로운 상태 객체를 만들어서 반환
@@ -17,7 +22,7 @@
 
 - **`index.js` : 리듀서 함수 정의, store 만들기, render 함수 만들기, 구독하기, 액션 발생시키기** 
 
-- 리덕스는 리액트에 종속되는 라이브러리가 아니다 => 다른 라이브러리/프레임워크에서도 사용할 수 있다 (따라서, `예제에서는 react 대신, vanilla javascript를 사용해 redux 사용` 예시를 보이고 있다)<br> react와 별개로 `yarn add redux` 설치 필요 
+- 리덕스는 리액트에 종속되는 라이브러리가 아니다 → 다른 라이브러리/프레임워크에서도 사용할 수 있다 (따라서, `예제에서는 react 대신, vanilla javascript를 사용해 redux 사용` 예시를 보이고 있다)<br> react와 별개로 `yarn add redux` 설치 필요 
 
 - 구독 : subscribe() 함수 안에 리스너 함수를 파라미터로 넣어 호출하면, 액션이 디스패치되어 상태가 업데이트될 때마다 호출<br> 스토어 내장 함수 중 하나<br> 
 
@@ -27,19 +32,19 @@
 
 - 리듀서 : 변화를 일으키는 함수<br> 액션을 만들어 발생 → 리듀서가 현재 상태, 액션 객체를 파라미터로 받아온다<br> 두 값을 이용해 새로운 상태를 만들어 반환한다
     ```javascript
-        const initialState = {
-            counter: 1
+    const initialState = {
+        counter: 1
+    }
+    function reducer(state = initialSTate, action) {
+        switch (action.type) {
+            case INCREMENT:
+                return {
+                    counter: state.counter + 1
+                };
+            default:
+                return state;
         }
-        function reducer(state = initialSTate, action) {
-            switch (action.type) {
-                case INCREMENT:
-                    return {
-                        counter: state.counter + 1
-                    };
-                default:
-                    return state;
-            }
-        }
+    }
     ```
 
 - 액션 생성 함수 : 액션 객체를 만드는 함수
@@ -60,7 +65,7 @@
     ```
 
 - 액션: 상태에 대한 변화가 필요할 때 발생
-    ```
+    ```javascript
     {
         type: 'ADD_TODO',
         data: {
--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -1,20 +1,24 @@
 import { createStore } from "redux";
 
-// DOM 레퍼런스 만들기
 const divToggle = document.querySelector(".toggle");
 const counter = document.querySelector("h1");
 const btnIncrease = document.querySelector("#increase");
 const btnDecrease = document.querySelector("#decrease");
 
-// 액션 이름 정의 cf. 액션: 프로젝트의 상태에 변화를 일으키는 것
+
+// 1-1. 액션 이름 정의(cf. 액션: 프로젝트의 상태에 변화를 일으키는 것)
 const TOGGLE_SWITCH = "TOGGLE_SWITCH";
 const INCREASE = "INCREASE";
 const DECREASE = "DECREASE";
 
-// 액션 생성 함수 작성: type을 반드시 가져야 함
+// 1-2. 액션 생성 함수 작성: *type을 반드시 가져야 함
+// 업데이트에 참고하고 싶은 값은 자유롭게 추가 가능 ex) difference
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = (difference) => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
+
+
+/* 2. 리듀서 함수 정의 cf. 리듀서: 변화를 일으키는 함수, 파라미터로 state, action 값을 받아온다 */
 
 // 초깃값 설정
 const initialState = {
@@ -22,17 +26,13 @@ const initialState = {
   counter: 0,
 };
 
-// 리듀서 함수 정의 cf. 리듀서: 변화를 일으키는 함수, 파라미터로 state, action 값을 받아온다
-
-// state가 undefined일 때는 initialState를 기본값으로 사용
-function reducer(state = initialState, action) {
-  // state의 초기 값은
-  // action.type에 따라 다른 작업을 처리
+function reducer(state = initialState, action) {  // state: 기존의 상태를 나타내는 관례적인 변수명, 현재 initialState를 기본값으로 설정
+  // action.type에 따라 state 값을 다르게 작업 처리
   switch (action.type) {
     case TOGGLE_SWITCH:
       return {
-        ...state, // 불변성 유지를 해줘야 한다
-        toggle: !state.toggle,
+        ...state, // 불변성 유지: ...state 복사 
+        toggle: !state.toggle,  // state 내부의 toggle 값 변경
       };
     case INCREASE:
       return {
@@ -49,26 +49,33 @@ function reducer(state = initialState, action) {
   }
 }
 
+
+// 3. store 만들기: 애플리케이션 상태, 리듀서, 내장함수 저장 공간, 하나의 프로젝트 하나의 스토어
 const store = createStore(reducer);
 
-// render: 상태가 업데이트 될 때마다 호출
-const render = () => {
-  const state = store.getState(); // 현재 상태를 불러오기
 
+// 4. render 함수 생성: 상태가 업데이트 될 때마다 호출하려는 작업
+const render = () => {
+  // 현재 상태를 불러오기
+  const state = store.getState(); 
   // 토글 처리
   if (state.toggle) {
     divToggle.classList.add("active");
   } else {
     divToggle.classList.remove("active");
   }
-
   // 카운터 처리
   counter.innerText = state.counter;
 };
 
-render();
-store.subscribe(render); // 상태가 업데이트될 때마다 render 함수 호출
+render();  // render 함수의 실행
 
+
+// 5. 구독하기
+store.subscribe(render); // 스토어의 상태가 업데이트될 때마다 render 함수 호출
+
+
+/* 1-3. 액션 발생 시키기 */
 divToggle.onclick = () => {
   store.dispatch(toggleSwitch());
 };
