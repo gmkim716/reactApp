4a32e8bb8c640e5e906bfd2c016cc5ffb2a9dc93
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2023-10-09 22:46:53 +0900

    error resolve

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -6,28 +6,31 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer, { rootSaga } from "./modules";
 import { Provider, createStoreHook } from "react-redux";
 import loggerMiddleware from "./lib/loggerMiddleware";
-import { createLogger } from 'redux-logger';
-import thunk from 'redux-thunk'
-import createSagaMiddleware from 'redux-saga';
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 /* 
   비동기 작업을 처리할 때 도움을 주는 미들웨어는 다양 
   실습에서 redux-thunk / redux-saga를 사용
 */
- 
+
 /* redux-saga 사용*/
 const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(logger, thunk, sagaMiddleware));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger, thunk, sagaMiddleware)
+);
 sagaMiddleware.run(rootSaga);
 
 // /* redux-thunk 사용: loggerMiddleware */
 // const logger = createLogger()
-// const store = createStore(rootReducer, applyMiddleware(logger, thunk)); 
+// const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 // /* redux-logger 사용: loggerMiddleware */
 // const logger = createLogger()
-// const store = createStore(rootReducer, applyMiddleware(logger)); 
+// const store = createStore(rootReducer, applyMiddleware(logger));
 
 // /* loggerMiddelware 사용 */
 // const store = createStore(rootReducer, applyMiddleware(loggerMiddleware)); // applyMiddleware: 미들웨어 적용
--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -1,66 +1,67 @@
-import { createAction, handleActions } from 'redux-actions';
-import { call, put, takeLatest } from 'reudx-saga/effects';
-import * as api from '../lib/api';
-import createRequestThunk from '../lib/createRequestThunk';
-import { startLoading, finishLoading } from './loading';
-import createRequestSaga from '../lib/createRequestSaga';
+import { createAction, handleActions } from "redux-actions";
+import { call, put, takeLatest } from "redux-saga/effects";
+import * as api from "../lib/api";
+import createRequestThunk from "../lib/createRequestThunk";
+import { startLoading, finishLoading } from "./loading";
+import createRequestSaga from "../lib/createRequestSaga";
 
 // 액션 타입을 선언
 // 한 요청당 세 개를 만들어야 한다
 
-const GET_POST = 'sample/GET_POST';
-const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
+const GET_POST = "sample/GET_POST";
+const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
+const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
 
-const GET_USERS = 'sample/GET_USERS';
-const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+const GET_USERS = "sample/GET_USERS";
+const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
+const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 // thunk 함수를 생성
 // thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치
 
-export const getPost = createAction(GET_POST, id => id);
+export const getPost = createAction(GET_POST, (id) => id);
 export const getUsers = createAction(GET_USERS);
 
-const getPostSaga = createRequestSaga(GET_POST, api.getPost);
-const getUserSaga = createRequestSaga(GET_USERS, api.getUsers);
-
+// const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+// const getUserSaga = createRequestSaga(GET_USERS, api.getUsers);
 
 function* getPostSaga(action) {
-  yield put(startLoading(GET_POST));  // 로딩 시작
+  yield put(startLoading(GET_POST)); // 로딩 시작
+
   // 파라미터로 action을 받아오면 액션의 정보를 조회할 수 있다
   try {
     // call을 사용하면 Promise를 반환하는 함수를 호출하고 기다릴 수 있다
     // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수
-    const post = yield call(api.getPost, action.payload);  // api.getPost(action.payload)를 의미
+    const post = yield call(api.getPost, action.payload); // api.getPost(action.payload)를 의미
     yield put({
       type: GET_POST_SUCCESS,
-      payload: post.data
+      payload: post.data,
     });
   } catch (e) {
     // try-catch문을 사용해 에러를 잡을 수 있다
     yield put({
       type: GET_POST_FAILURE,
       payload: e,
-      error: true
+      error: true,
     });
   }
-  yield put(finishLoading(GET_POST));  // 로딩 완료
+
+  yield put(finishLoading(GET_POST)); // 로딩 완료
 }
 
 function* getUserSaga() {
   yield put(startLoading(GET_USERS));
   try {
-    const post = yield call(api.getUsers);  
+    const post = yield call(api.getUsers);
     yield put({
       type: GET_POST_SUCCESS,
-      payload: post.data
+      payload: post.data,
     });
   } catch (e) {
     yield put({
       type: GET_POST_FAILURE,
       payload: e,
-      error: true
+      error: true,
     });
   }
   yield put(finishLoading(GET_USERS));
@@ -75,14 +76,13 @@ export function* sampleSaga() {
 // export const getPost = createRequestThunk(GET_POST, api.getPost);
 // export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
-
 // /* 리팩토리 이전: 기존 thunk 함수 & 로딩 상태를 리듀서에서 관리 방법 */
 // export const getPost = id => async dispatch => {
 //   dispatch({ type: GET_POST });  // 요청을 시작한 것을 알림
 //   try {
 //     const response = await api.getPost(id);
 //     dispatch({ type: GET_POST_SUCCESS, payload: response.data })  // 요청 성공
-//   } 
+//   }
 //   catch (e) {
 //     dispatch({ type: GET_POST_FAILURE, payload: e, error: true });  // 에러 발생
 //     throw e;  // 컴포넌트 단에서 에러를 조회할 수 있도록 함
@@ -93,7 +93,7 @@ export function* sampleSaga() {
 //   try {
 //     const response = await api.getUsers(id);
 //     dispatch({ type: GET_USERS_SUCCESS, payload: response.data })  // 요청 성공
-//   } 
+//   }
 //   catch (e) {
 //     dispatch({ type: GET_USERS_FAILURE, payload: e, error: true });  // 에러 발생
 //     throw e;  // 컴포넌트 단에서 에러를 조회할 수 있도록 함
@@ -109,7 +109,7 @@ const initialState = {
   //   GET_USERS: false
   // },
   post: null,
-  users: null
+  users: null,
 };
 
 const sample = handleActions(
@@ -117,50 +117,50 @@ const sample = handleActions(
     // [GET_POST]: state => ({
     //   ...state,
     //   loading: {
-    //     ...state.loading, 
-    //     GET_POST: true  // 요청 시작 
+    //     ...state.loading,
+    //     GET_POST: true  // 요청 시작
     //   }
     // }),
 
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
       // loading: {
-      //   ...state.loading, 
-      //   GET_POST: false  // 요청 완료 
+      //   ...state.loading,
+      //   GET_POST: false  // 요청 완료
       // },
-      post: action.payload
+      post: action.payload,
     }),
 
     // [GET_POST_FAILURE]: (state, action) => ({
     //   ...state,
     //   loading: {
-    //     ...state.loading, 
-    //     GET_POST: false  // 요청 완료 
+    //     ...state.loading,
+    //     GET_POST: false  // 요청 완료
     //   }
     // }),
 
     // [GET_USERS]: state => ({
     //   ...state,
     //   loading: {
-    //     ...state.loading, 
-    //     GET_USERS: true  // 요청 시작 
+    //     ...state.loading,
+    //     GET_USERS: true  // 요청 시작
     //   }
     // }),
 
     [GET_USERS_SUCCESS]: (state, action) => ({
       ...state,
       // loading: {
-      //   ...state.loading, 
-      //   GET_USERS: false  // 요청 완료 
+      //   ...state.loading,
+      //   GET_USERS: false  // 요청 완료
       // },
-      users: action.payload
+      users: action.payload,
     }),
 
     // [GET_USERS_FAILURE]: (state, action) => ({
     //   ...state,
     //   loading: {
-    //     ...state.loading, 
-    //     GET_USERS: false  // 요청 완료 
+    //     ...state.loading,
+    //     GET_USERS: false  // 요청 완료
     //   }
     // })
   },
