94507cb8dfcb234c3e1e48442ebb66686c1fe343
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2023-10-09 23:12:32 +0900

    end async task management using redux middleware

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -1,10 +1,17 @@
 import { createAction, handleActions } from "redux-actions";
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import {
+  delay,
+  put,
+  takeEvery,
+  takeLatest,
+  select,
+  throttle,
+} from "redux-saga/effects";
 
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
-const INCREASE_ASYNC = 'counter/INCREASE_ASYNC';
-const DECREASE_ASYNC = 'counter/DECREASE_ASYNC';
+const INCREASE_ASYNC = "counter/INCREASE_ASYNC";
+const DECREASE_ASYNC = "counter/DECREASE_ASYNC";
 
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
@@ -12,16 +19,24 @@ export const increaseAsync = createAction(INCREASE_ASYNC);
 export const decreaseAsync = createAction(DECREASE_ASYNC);
 
 function* increaseSaga() {
-  yield delay(1000);  // 1초 대기
-  yield put(increase());  // 특정 액션을 디스패치
+  yield delay(1000); // 1초 대기
+  yield put(increase()); // 특정 액션을 디스패치
+
+  // *redux-saga에서 현재 상태를 조회하는 방법: select 사용
+  const number = yield select((state) => state.counter); // state는 스토어 상태를 의미
+  console.log(`현재 값은 ${number}입니다.`);
 }
 function* decreaseSaga() {
-  yield delay(1000);  // 1초 대기
-  yield put(decrease());  // 특정 액션을 디스패치
+  yield delay(1000); // 1초 대기
+  yield put(decrease()); // 특정 액션을 디스패치
 }
 export function* counterSaga() {
-  // takeEvery: 들어오는 모든 액션에 대해 특정 작업을 처리
-  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  // // takeEvery: 들어오는 모든 액션에 대해 특정 작업을 처리
+  // yield takeEvery(INCREASE_ASYNC, increaseSaga);
+
+  // throttle: takeEvery 대신 사용, n초에 단 한번만 호출되도록 설정
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
+
   // takeLatest: 기존 작업 중인 내용 취소 처리, 가장 마지막으로 시랳ㅇ된 작업만 수행
   yield takeLatest(DECREASE_ASYNC, decreaseSaga);
 }
@@ -39,7 +54,7 @@ export function* counterSaga() {
 //   }, 1000);
 // }
 
-const initialState = 0;  // 상태는 꼭 객체일 필요가 없으며 숫자도 작동한다
+const initialState = 0; // 상태는 꼭 객체일 필요가 없으며 숫자도 작동한다
 
 const counter = handleActions(
   {
--- learn-redux-middleware/src/modules/sample.js
+++ learn-redux-middleware/src/modules/sample.js
@@ -22,50 +22,50 @@ const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 export const getPost = createAction(GET_POST, (id) => id);
 export const getUsers = createAction(GET_USERS);
 
-// const getPostSaga = createRequestSaga(GET_POST, api.getPost);
-// const getUserSaga = createRequestSaga(GET_USERS, api.getUsers);
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUserSaga = createRequestSaga(GET_USERS, api.getUsers);
 
-function* getPostSaga(action) {
-  yield put(startLoading(GET_POST)); // 로딩 시작
+// function* getPostSaga(action) {
+//   yield put(startLoading(GET_POST)); // 로딩 시작
 
-  // 파라미터로 action을 받아오면 액션의 정보를 조회할 수 있다
-  try {
-    // call을 사용하면 Promise를 반환하는 함수를 호출하고 기다릴 수 있다
-    // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수
-    const post = yield call(api.getPost, action.payload); // api.getPost(action.payload)를 의미
-    yield put({
-      type: GET_POST_SUCCESS,
-      payload: post.data,
-    });
-  } catch (e) {
-    // try-catch문을 사용해 에러를 잡을 수 있다
-    yield put({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
+//   // 파라미터로 action을 받아오면 액션의 정보를 조회할 수 있다
+//   try {
+//     // call을 사용하면 Promise를 반환하는 함수를 호출하고 기다릴 수 있다
+//     // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수
+//     const post = yield call(api.getPost, action.payload); // api.getPost(action.payload)를 의미
+//     yield put({
+//       type: GET_POST_SUCCESS,
+//       payload: post.data,
+//     });
+//   } catch (e) {
+//     // try-catch문을 사용해 에러를 잡을 수 있다
+//     yield put({
+//       type: GET_POST_FAILURE,
+//       payload: e,
+//       error: true,
+//     });
+//   }
 
-  yield put(finishLoading(GET_POST)); // 로딩 완료
-}
+//   yield put(finishLoading(GET_POST)); // 로딩 완료
+// }
 
-function* getUserSaga() {
-  yield put(startLoading(GET_USERS));
-  try {
-    const users = yield call(api.getUsers);
-    yield put({
-      type: GET_USERS_SUCCESS,
-      payload: users.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_USERS));
-}
+// function* getUserSaga() {
+//   yield put(startLoading(GET_USERS));
+//   try {
+//     const users = yield call(api.getUsers);
+//     yield put({
+//       type: GET_USERS_SUCCESS,
+//       payload: users.data,
+//     });
+//   } catch (e) {
+//     yield put({
+//       type: GET_USERS_FAILURE,
+//       payload: e,
+//       error: true,
+//     });
+//   }
+//   yield put(finishLoading(GET_USERS));
+// }
 
 export function* sampleSaga() {
   yield takeLatest(GET_POST, getPostSaga);
