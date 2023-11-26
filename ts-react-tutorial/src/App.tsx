50045439a47d5bdca2e2ffcc507760bc517f5ec5
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2023-11-27 01:08:50 +0900

    manage input state

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import Greetings from "./Greetings";
+import Greetings from "./components/Greetings";
+import Counter from "./components/Counter";
+import MyForm from "./components/MyForm";
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
 
-  return <Greetings name="Jayden" onClick={onClick} />;
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+
+  return (
+    <>
+      <Greetings name="Jayden" onClick={onClick} />
+      <hr />
+      <Counter />
+      <hr />
+      <MyForm onSubmit={onSubmit} />
+    </>
+  );
 };
 
 export default App;
